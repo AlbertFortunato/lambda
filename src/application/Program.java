55d821f39444b526906ca30da3ad602e7d33de76
Albert Fortunato <f.ortunato@hotmail.com>
2021-11-22 09:52:20 -0300

    Price Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
 
 import entities.Product;
-import util.ProductPredicate;
+import util.PriceUpdate;
 
 public class Program {
 
@@ -18,12 +17,10 @@ public class Program {
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
-        
 
-        list.removeIf(p -> p.getPrice() >= 100);
+        list.forEach(Product::staticPriceUpdate);
 
-        for ( Product p : list){
-            System.out.println(p);
-        }
+        list.forEach(System.out::println);
+
     }
 }
