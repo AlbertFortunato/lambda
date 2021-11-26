2e19ee73e8097c8567dbda96f639d73e7808425c
Albert Fortunato <f.ortunato@hotmail.com>
2021-11-26 14:44:03 -0300

    Lambda

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,12 +3,11 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
-import java.util.function.Function;
+
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
+import model.ProductService;
 
 public class Program {
 
@@ -21,10 +20,11 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
+        ProductService ps = new ProductService();
 
-        List <String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+        double sum = ps.filteredSum(list,p -> p.getName().charAt(0) == 'T');
 
-        names.forEach(System.out::println);
+        System.out.println("Sum = " + String.format("%.2f", sum));
 
     }
 }
